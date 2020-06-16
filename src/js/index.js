dc1d37b8604fd7d0e172927b008f3d8d7bf5e370
Kirill <kirill.morozov_1994@mail.ru>
2020-06-16 20:38:06 +0300

    Add overlay for device

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,27 @@
-import '@scss/main.scss';
-import ScrollingPage from '@parts/scrolling-page';
-import ScrollingLinks from '@parts/scrolling-links';
+import "@scss/main.scss";
+import ScrollingPage from "@parts/scrolling-page";
+import ScrollingLinks from "@parts/scrolling-links";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new ScrollingPage(".container");
 
-	const app = new ScrollingPage('.container');
+  app.run().addNextScroll(".arrow-down__object");
 
-	app.run().addNextScroll('.arrow-down__object');
+  const links = new ScrollingLinks({
+    wrapper: ".part-3__wrapper",
+    target: ".range",
+  });
 
-	const links = new ScrollingLinks({
-		wrapper: '.part-3__wrapper',
-		target: '.range'
-	})
+  links.run();
 
-	links.run();
+  const windowSize = document.querySelector(".window-size");
+  if (window.innerWidth !== 1024) windowSize.style.display = "flex";
+
+  window.addEventListener("resize", (e) => {
+    if (e.target.innerWidth !== 1024) {
+      windowSize.style.display = "flex";
+    } else {
+      windowSize.style.display = "none";
+    }
+  });
 });
--- src/js/parts/scrolling-links.js
+++ src/js/parts/scrolling-links.js
@@ -1,49 +1,49 @@
 export default class ScrollingLinks {
-	constructor({ wrapper, target }) {
-		this.target = document.querySelector(target);
-		this.wrapper = document.querySelector(wrapper);
-		this.links = this.wrapper.children;
-		this.currentSlide = 2;
-	}
+  constructor({ wrapper, target }) {
+    this.target = document.querySelector(target);
+    this.wrapper = document.querySelector(wrapper);
+    this.links = this.wrapper.children;
+    this.currentSlide = 2;
+  }
 
-	run() {
-		this.setInitData();
-		this.setSlide();
-		this.target.addEventListener('input', this.setLink.bind(this));
-	}
+  run() {
+    this.setInitData();
+    this.setSlide();
+    this.target.addEventListener("input", this.setLink.bind(this));
+  }
 
-	setLink(e) {
-		this.choicePeriod(e.target.value, 1);
-		this.setSlide();
-	}
+  setLink(e) {
+    this.choicePeriod(e.target.value, 1);
+    this.setSlide();
+  }
 
-	choicePeriod(value, countLink) {
-		if(value > (100/this.links.length)*countLink) {
-			this.choicePeriod(value, countLink + 1);
-		} else {
-			this.currentSlide = countLink - 1;
-		}
-	}
+  choicePeriod(value, countLink) {
+    if (value > (100 / this.links.length) * countLink) {
+      this.choicePeriod(value, countLink + 1);
+    } else {
+      this.currentSlide = countLink - 1;
+    }
+  }
 
-	setInitData() {
-		this.wrapper.style.cssText = `
-			transform: translateX(${100 / this.links.length * this.currentSlide}%));
+  setInitData() {
+    this.wrapper.style.cssText = `
+			transform: translateX(${(100 / this.links.length) * this.currentSlide}%));
 		`;
-		Array.prototype.forEach.call(this.links, (link) => {
-			link.style.cssText = `
+    Array.prototype.forEach.call(this.links, (link) => {
+      link.style.cssText = `
 				width: ${100 / this.links.length}%;
 				height: 100%;
 			`;
-		});
-	}
+    });
+  }
 
-	setSlide() {
-		const value = (this.target.value/100)*100;
-		this.target.style.cssText = `
+  setSlide() {
+    const value = (this.target.value / 100) * 100;
+    this.target.style.cssText = `
 			background-image: linear-gradient(to right, #d1eaff 0%, #d1eaff ${value}%, #c8c8c8 ${value}%, #c8c8c8 100%);
 		`;
-		this.wrapper.style.cssText = `
-			transform: translateX(${-100 / this.links.length * this.currentSlide}%);
+    this.wrapper.style.cssText = `
+			transform: translateX(${(-100 / this.links.length) * this.currentSlide}%);
 		`;
-	}
+  }
 }
